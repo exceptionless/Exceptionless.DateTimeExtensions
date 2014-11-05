08c161fda5b8e0a7c07341e82f0b329ee4eea31b
Eric J. Smith <eric@codesmithtools.com>
2014-11-05 01:32:26 -0600

    Add a nuspec file.

--- Source/Properties/AssemblyInfo.cs
+++ Source/Properties/AssemblyInfo.cs
@@ -1,33 +1,19 @@
 ﻿using System.Resources;
 using System.Reflection;
 using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("DateTimeRange")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Exceptionless.DateTimeExtensions")]
+[assembly: AssemblyDescription("DateTimeRange and various DateTime and TimeSpan extension methods.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DateTimeRange")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCompany("Exceptionless")]
+[assembly: AssemblyProduct("Exceptionless")]
+[assembly: AssemblyCopyright("Copyright (c) 2014 Exceptionless.  All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyFileVersion("3.0.0.0")]
 
 [assembly: InternalsVisibleTo("Exceptionless.DateTimeExtensions.Tests")]
 
