2d2b72b78333d3f23a7446c4dd0cbf55d639de05
Blake Niemyjski <bniemyjski@gmail.com>
2016-12-15 17:48:35 -0600

    Updated the expected values from nano seconds

--- tests/Exceptionless.DateTimeExtensions.Tests/TimeUnitTests.cs
+++ tests/Exceptionless.DateTimeExtensions.Tests/TimeUnitTests.cs
@@ -6,10 +6,10 @@ using Xunit;
 namespace Exceptionless.DateTimeExtensions.Tests {
     public class TimeUnitTests {
         public static IEnumerable<object[]> TestData => new[] {
-            new object[] { "1000 nanos", new TimeSpan(100) },
-            new object[] { "1000nanos", new TimeSpan(100) },
-            new object[] { "1000 NANOS", new TimeSpan(100) },
-            new object[] { "1000NANOS", new TimeSpan(100) },
+            new object[] { "1000 nanos", new TimeSpan(10) },
+            new object[] { "1000nanos", new TimeSpan(10) },
+            new object[] { "1000 NANOS", new TimeSpan(10) },
+            new object[] { "1000NANOS", new TimeSpan(10) },
             new object[] { "10micros", new TimeSpan(100) },
             new object[] { "10ms", new TimeSpan(0, 0, 0, 0, 10) },
             new object[] { "10s", new TimeSpan(0, 0, 10) },
