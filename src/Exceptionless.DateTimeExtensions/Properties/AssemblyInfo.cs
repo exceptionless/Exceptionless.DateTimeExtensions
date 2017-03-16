057792650b6b00ba0399cc1ac605ecc3f15bea7c
Eric J. Smith <eric@codesmithtools.com>
2017-03-16 16:32:26 -0500

    Update project structure and move to csproj

--- src/Exceptionless.DateTimeExtensions/Properties/AssemblyInfo.cs
+++ src/Exceptionless.DateTimeExtensions/Properties/AssemblyInfo.cs
@@ -1,18 +1,3 @@
-﻿using System.Resources;
-using System.Reflection;
-using System.Runtime.CompilerServices;
-
-[assembly: AssemblyTitle("Exceptionless.DateTimeExtensions")]
-[assembly: AssemblyDescription("DateTimeRange and various DateTime and TimeSpan extension methods.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Exceptionless")]
-[assembly: AssemblyProduct("Exceptionless")]
-[assembly: AssemblyCopyright("Copyright (c) 2014 Exceptionless.  All rights reserved.")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("en")]
-
-[assembly: AssemblyVersion("3.0.0.0")]
-[assembly: AssemblyFileVersion("3.0.0.0")]
+﻿using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("Exceptionless.DateTimeExtensions.Tests")]
