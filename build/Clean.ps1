8bb9f35dc6e51f5138c534d437b27d9d2cce1c5f
Eric J. Smith <eric@codesmithtools.com>
2017-08-15 23:19:44 -0500

    Fix artifacts

---
+++ build/Clean.ps1
@@ -0,0 +1,14 @@
+$root_dir = Resolve-Path "$PSScriptRoot\..\"
+Push-Location $root_dir
+
+Remove-Item $root_dir\artifacts -Force -Recurse
+
+Get-ChildItem $root_dir\src -Include bin,obj -Recurse |
+    ForEach-Object ($_) {
+        Remove-Item $_.FullName -Force -Recurse
+    }
+
+Get-ChildItem $root_dir\test -Include bin,obj -Recurse |
+    ForEach-Object ($_) {
+        Remove-Item $_.FullName -Force -Recurse
+    }
